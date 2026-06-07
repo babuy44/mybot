c2eea8117f07249165cc884009426f67626389b2
babuy44 <lloh7644@gmail.com>
2026-06-07 11:00:12 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -226,7 +226,7 @@ async def start(event):
         '• To withdraw funds — complete verification with /verify first\n'
         '• For more information — use /about',
         buttons=[
-            [Button.url('🚀 Open BlueVault App', 'https://mybot-production-702e.up.railway.app')]
+            [Button.url('🚀 Open BlueVault App', 'https://t.me/BlueVaultt_bot?profile')]
         ]
     )
 
